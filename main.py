59d94ddda0528c5210b2ba18aecaa9907b9dafb0
nikitt-code <nikitt.russia@gmail.com>
2021-01-17 15:55:07 +0500

    Add random ints gen.

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from DHLib import Encrypt
+from random import randint
 
 if __name__ == '__main__':
     enc = Encrypt()
@@ -9,15 +10,15 @@ if __name__ == '__main__':
     one_byte: int = 2 ** (256 * 8)
 
     # init private
-    Alice_a = one_byte * 134512125512  # be cool and use random values
-    Bob_b = one_byte * 512581052108  # be cool and use random values
+    Alice_a = one_byte * randint(100000000000, 999999999999)  # be cool and use random values
+    Bob_b = one_byte * randint(100000000000, 999999999999)  # be cool and use random values
 
     # init public p
-    Alice_p = one_byte * 85125125125  # be cool and use random values
+    Alice_p = one_byte * randint(100000000000, 999999999999)  # be cool and use random values
     Bob_p = Alice_p
 
     # init public g
-    Bob_g = one_byte * 120521512551  # be cool and use random values
+    Bob_g = one_byte * randint(100000000000, 999999999999)  # be cool and use random values
     Alice_g = Bob_g
 
     # create public A, B
